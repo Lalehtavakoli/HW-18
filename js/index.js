defa331625686c42d5ce369f53781f18c7c4f95f
Lalehtavakoli <lalehtavakoli58@gmail.com>
2024-02-11 17:56:12 -0500

    updated

--- js/index.js
+++ js/index.js
@@ -1,25 +1,28 @@
 console.log("Your index.js file is loaded correctly!");
 
-
 $(document).ready(function() {
-    var learnMoreBtn = $("#learnMoreBtn");
-  
-    // When the user scrolls down 20px from the top of the document, show the button
-    $(window).scroll(function() {
-      scrollFunction();
-    });
-  
-    function scrollFunction() {
-      if ($(document).scrollTop() > 20) {
-        mybutton.css("display", "block");
-      } else {
-        mybutton.css("display", "none");
-      }
-    }
-  
-    // When the user clicks on the button, scroll to the top of the document
-    mybutton.click(function() {
-      $("body,html").animate({ scrollTop: 0 }, "slow");
-    });
+  var learnMoreBtn = $("#learnMoreBtn");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  $(window).scroll(function() {
+    scrollFunction();
   });
+
+  function scrollFunction() {
+    if ($(document).scrollTop() > 20) {
+      mybutton.css("display", "block");
+    } else {
+      mybutton.css("display", "none");
+    }
+  }
+
   
+  'downloadResume'.click(function() {
+    $("body,html").animate({ scrollTop: 0 }, "slow");
+  });
+});
+
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Thank you for downloading my resume!";
+}
